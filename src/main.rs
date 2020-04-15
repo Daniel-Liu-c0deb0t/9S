930e6b8e6f78f153d2248f59bc7f153a442f9c91
c0deb0t <daniel.liu02@gmail.com>
2020-04-14 21:58:08 -0700

    Finished basic ping

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 use std::env;
 use std::net;
-use std::str::FromStr;
+use std::str::*;
+
+use pnet::packet::*;
+use pnet::transport::*;
+
+const PAYLOAD_SIZE: usize = 56;
+const DELAY_MILLIS: u64 = 500;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -41,90 +47,83 @@ fn main() {
     println!("Pinging {} ({}) with 9S's special abilities.", addr, ip_addr);
 
     match ip_addr {
-        V4(ip) => {
+        net::IpAddr::V4(_) => {
             // note: must use Layer4 since pnet does not support IPv6 Layer3
-            let (mut sender, mut reciever) =
-                transport_channel(100, TransportChannelType::Layer4(TransportProtocol::Ipv4(IpNextHeaderProtocols::Icmp)))
+            let (mut sender, mut receiver) = transport_channel(100, TransportChannelType::Layer4(
+                    TransportProtocol::Ipv4(ip::IpNextHeaderProtocols::Icmp)))
                 .expect("Unable to open transport channel! 9S unhappy!");
+            sender.set_ttl(ttl).unwrap();
+            let mut receiver_iter = icmp_packet_iter(&mut receiver);
 
             loop {
-                let packet = make_packet_ipv4(ip, ttl);
-                sender.send_to(packet, ip_addr);
+                let mut icmp_buffer = [0u8; 8 + PAYLOAD_SIZE];
+                let packet = make_icmp_packet(&mut icmp_buffer);
+                sender.send_to(packet, ip_addr).expect("Error in sending packet! 9S unhappy!");
 
-                let res_packet = reciever.next().expect("Error in recieving next packet! 9S unhappy!");
+                loop {
+                    let res_packet = receiver_iter.next().expect("Error in receiving next packet! 9S unhappy!");
 
+                    if res_packet.0.get_icmp_type() == icmp::IcmpTypes::EchoReply {
+                        println!("{:?}", res_packet);
 
+                        break;
+                    }
+                }
+
+                std::thread::sleep(std::time::Duration::from_millis(DELAY_MILLIS));
             }
         },
-        V6(ip) => {
+        net::IpAddr::V6(ip) => {
+            // note: must use Layer4 since pnet does not support IPv6 Layer3
+            let (mut sender, mut receiver) = transport_channel(100, TransportChannelType::Layer4(
+                    TransportProtocol::Ipv6(ip::IpNextHeaderProtocols::Icmpv6)))
+                .expect("Unable to open transport channel! 9S unhappy!");
+            sender.set_ttl(ttl).unwrap();
+            let mut receiver_iter = icmpv6_packet_iter(&mut receiver);
 
+            loop {
+                let mut icmp_buffer = [0u8; 8 + PAYLOAD_SIZE];
+                let packet = make_icmpv6_packet(ip, &mut icmp_buffer);
+                sender.send_to(packet, ip_addr).expect("Error in sending packet! 9S unhappy!");
+
+                loop {
+                    let res_packet = receiver_iter.next().expect("Error in receiving next packet! 9S unhappy!");
+
+                    if res_packet.0.get_icmpv6_type() == icmpv6::Icmpv6Types::EchoReply {
+                        println!("{:?}", res_packet);
+
+                        break;
+                    }
+                }
+            }
         }
+    }
 }
 
-fn make_packet_ipv4(dest: net::Ipv4Addr, ttl: u8) -> Ipv4Packet {
-    let mut ipv4_packet = ipv4::MutableIpv4Packet(&mut [0u8; 100]).unwrap();
-    ipv4packet.populate(&ipv4::Ipv4{
-        version: 4,
-        header_length: 20,
-        dscp: 0,
-        ecn: 0,
-        total_length: 20 + 8 + 64,
-        identification: 0,
-        flag: 0,
-        fragment_offset: 0,
-        ttl: ttl,
-        next_level_protocol: IpNextHeaderProtocols::Icmp,
+fn make_icmp_packet(icmp_buffer: &mut [u8]) -> icmp::IcmpPacket {
+    let mut icmp_packet = icmp::MutableIcmpPacket::new(icmp_buffer).unwrap();
+    icmp_packet.populate(&icmp::Icmp{
+        icmp_type: icmp::IcmpTypes::EchoRequest,
+        icmp_code: icmp::IcmpCode::new(0),
         checksum: 0,
-        source: Ipv4Addr::from_str("0.0.0.0"),
-        destination: dest,
-        options: vec![],
         payload: vec![]
     });
 
-    let mut icmp_packet = icmp::echo_request::MutableEchoRequestPacket(&mut [0u8; 8 + 64]).unwrap();
-    icmp_packet.populate(&icmp::echo_request::EchoRequest{
-        icmp_type: IcmpTypes::EchoRequest,
-        icmp_code: icmp::echo_request::IcmpCodes::NoCode,
-        checksum: 0,
-        identifier: 0,
-        sequence_number: 0,
-        payload: vec![]
-    });
-
-    icmp_packet.set_checksum(icmp::checksum(&icmp_packet));
-    ipv4_packet.set_payload(icmp_packet.packet());
-    ipv4_packet.set_checksum(ipv4::checksum(&ipv4_packet));
-
-    ipv4_packet.consume_to_immutable()
+    icmp_packet.set_checksum(icmp::checksum(&icmp_packet.to_immutable()));
+    icmp_packet.consume_to_immutable()
 }
 
-fn make_packet_ipv6(dest: net::Ipv6Addr, hop_limit: u8) -> Ipv6Packet {
-    let mut ipv6_packet = ipv4::MutableIpv6Packet(&mut [0u8; 100]).unwrap();
-    ipv6packet.populate(&ipv6::Ipv6{
-        version: 6,
-        traffic_class: 0,
-        flow_label: 0,
-        payload_length: 64,
-        next_header: IpNextHeaderProtocols::Icmp,
-        hop_limit: hop_limit,
-        checksum: 0,
-        source: Ipv6Addr::from_str("0.0.0.0.0.0"),
-        destination: dest,
-        payload: vec![]
-    });
-
-    let mut icmp_packet = icmpv6::MutableIcmpv6Packet(&mut [0u8; 8 + 64]).unwrap();
+fn make_icmpv6_packet(dest: net::Ipv6Addr, icmp_buffer: &mut [u8]) -> icmpv6::Icmpv6Packet {
+    let mut icmp_packet = icmpv6::MutableIcmpv6Packet::new(icmp_buffer).unwrap();
     icmp_packet.populate(&icmpv6::Icmpv6{
-        icmp_type: Icmpv6Types::EchoRequest,
-        icmp_code: icmpv6::ndp::IcmpCodes::NoCode,
+        icmpv6_type: icmpv6::Icmpv6Types::EchoRequest,
+        icmpv6_code: icmpv6::ndp::Icmpv6Codes::NoCode,
         checksum: 0,
         payload: vec![]
     });
 
-    icmp_packet.set_checksum(icmpv6::checksum(&icmp_packet));
-    ipv6_packet.set_payload(icmp_packet.packet());
-    ipv6_packet.set_checksum(ipv6::checksum(&ipv6_packet));
-
-    ipv6_packet.consume_to_immutable()
+    icmp_packet.set_checksum(icmpv6::checksum(
+            &icmp_packet.to_immutable(), &net::Ipv6Addr::from_str("0.0.0.0.0.0").unwrap(), &dest));
+    icmp_packet.consume_to_immutable()
 }
 
